bac15488d0a7a68720b36c3df4291ea7b27adb04
Yordan Miladinov <jordanMiladinov@gmail.com>
2021-06-07 21:49:21 +0300

    the order struct needs just quantity and id, everything else would be handled outside

--- order.go
+++ order.go
@@ -15,7 +15,6 @@ const (
 )
 
 type Order struct {
-	Side     int             //  4 bytes
 	Quantity decimal.Decimal //  8 bytes
 	ID       string          // 16 bytes
-} //            Total: at least 28 bytes
+} //                Total: at least 24 bytes
