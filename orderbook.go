9a4f8e51bba2cf7db6ae558f8526566d6519f8de
Yordan Miladinov <jordanMiladinov@gmail.com>
2021-06-08 21:46:27 +0300

    struct Book stub

--- orderbook.go
+++ orderbook.go
@@ -1,3 +1,18 @@
 // Package orderbook represents a simplified order book.  It supports
 // submitting, canceling, querying and matching orders.
 package orderbook
+
+type Book struct {
+	Asks Ladder
+	Bids Ladder
+}
+
+func NewBook() *Book {
+	return &Book{
+		Asks: NewLadder(Ask),
+		Bids: NewLadder(Bid),
+	}
+}
+
+// func (b *Book) AddOrder(orderType int) {
+// }
