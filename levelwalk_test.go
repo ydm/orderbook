66155c9c7e296d1bb06aef65286c1f8ca3dca5b1
Yordan Miladinov <jordanmiladinov@gmail.com>
2021-10-07 10:09:34 +0300

    improve levelwalk tests

--- levelwalk_test.go
+++ levelwalk_test.go
@@ -1,9 +1,35 @@
-package orderbook
+package orderbook_test
 
 import (
+	"container/heap"
 	"testing"
+
+	"github.com/shopspring/decimal"
+	"github.com/ydm/orderbook"
 )
 
 func TestLevelHeap_Walk(t *testing.T) {
-	// xs := NewLevelHeap(16)
+	t.Parallel()
+
+	f := func(levelType int, expected []string) {
+		xs := orderbook.NewLevelHeap(16)
+		heap.Push(&xs, orderbook.NewLevel(decimal.NewFromInt(4), levelType))
+		heap.Push(&xs, orderbook.NewLevel(decimal.NewFromInt(2), levelType))
+		heap.Push(&xs, orderbook.NewLevel(decimal.NewFromInt(5), levelType))
+		heap.Push(&xs, orderbook.NewLevel(decimal.NewFromInt(1), levelType))
+		heap.Push(&xs, orderbook.NewLevel(decimal.NewFromInt(3), levelType))
+
+		index := 0
+		xs.Walk(func(level *orderbook.Level) bool {
+			t.Helper()
+			if level.Price.String() != expected[index] {
+				t.Errorf("have %v, want %s", level.Price, expected[index])
+			}
+			index++
+			return true
+		})
+	}
+
+	f(orderbook.Ask, []string{"1", "2", "3", "4", "5"})
+	f(orderbook.Bid, []string{"5", "4", "3", "2", "1"})
 }
